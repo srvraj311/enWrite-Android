66e0a97ef69e306c9ca7a6a1dbf420e2b000a05b
srvraj311 <sourabhraj311@gmail.com>
2021-01-29 07:42:40 +0530

    Added Colour Picker in the New Note Activity - Alignment Required Added Delete Function in Recycler View Bug: Notes Don't Get auto refreshed after saving.. VVI

--- app/src/main/java/com/srvraj311/RecyclerAdapter.java
+++ app/src/main/java/com/srvraj311/RecyclerAdapter.java
@@ -1,5 +1,7 @@
 package com.srvraj311;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +10,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.circularreveal.cardview.CircularRevealCardView;
 import com.google.common.collect.Comparators;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.srvraj311.Note;
 import com.srvraj311.R;
 
@@ -22,11 +31,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.Objects;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>{
 
     private ArrayList<Note> notesArr;
-    CircularRevealCardView cardView;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     
     // Constructor to get the array of items to this class
     public RecyclerAdapter(ArrayList<Note> NotesArr){
@@ -49,6 +61,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(@NonNull RecyclerAdapter.MyViewHolder holder, int position) {
         String title = notesArr.get(position).getNote_title();
@@ -65,19 +78,58 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
             }
         });
         //CLICK ON CARD
-        holder.cardView.setOnLongClickListener(new View.OnLongClickListener() {
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-                return true;
+            public void onClick(View v) {
+                delete(position);
             }
         });
+
+
+        //Colour of the Holder
+        String color = notesArr.get(position).getNote_colour();
+        holder.cardView.setCardBackgroundColor(Color.parseColor(color));
+
+
+    }
+
+    private void delete(int position) {
+        this.notesArr.remove(position);
+
+
+
+        //Deleting Note from Firebase
+        String email = Objects.requireNonNull(mAuth.getCurrentUser()).getEmail();
+
+        CollectionReference users = db.collection("users");
+        assert email != null;
+        int notesCount = notesArr.size();
+        users.document(email).update("notes",this.notesArr,"notesCount", notesCount).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Log.e("DELETE","Succes");
+                }else {
+                    Log.e("DELETE","Failed");
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("DELETING NOTE", "Failed" + e);
+            }
+        });
+
+
+
+
+        this.setData(notesArr);
     }
 
     public void setData(ArrayList<Note> data){
         Collections.sort(data, new NoteComparator());
         this.notesArr = data;
         notifyItemChanged(notesArr.size(),null);
-//        notifyDataSetChanged();
     }
 
     @Override
